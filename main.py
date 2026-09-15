4ca8a021e68c4fd3965e17fdf73a5bed3b30ab59
CKLucas01 <lucasvandeweijerschool@gmail.com>
2026-09-15 12:08:17 +0200

    added multiplier

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from functions import *
 #window
 root = tk.Tk()
 root.title("Make Money")
+root.geometry("300x200")
 
 Moneylabel = tk.Label(root, text= "Money: 0$")
 Moneylabel.pack(pady=10)
+
+Multiplierlabel = tk.Label(root, text= "Multiplier: 1")
+Multiplierlabel.pack(pady=10)
 
 EarnButton = tk.Button(root, text="Earn", command=lambda: earn(Moneylabel))
 EarnButton.pack(pady=10)
